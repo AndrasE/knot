a783ac0ccaac147f8c14d74f5f21ca041708477d
AndrasE <rohadtsajt@gmail.com>
2025-09-17 18:14:47 +0100

    Update HeroSection.tsx

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -23,7 +23,7 @@ export default function HeroSection() {
         <img
           src={Us}
           alt="us"
-          className="w-3/4 rounded-2xl sm:w-1/3 max-w-[300px]"
+          className="w-3/4 rounded-2xl sm:w-1/3 max-w-[300px] shadow-xl"
         />
       </div>
     </section>
