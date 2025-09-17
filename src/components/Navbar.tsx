efbe10fd5ba3e06f01f0f5d06a13b9df5e1d7912
AndrasE <rohadtsajt@gmail.com>
2025-09-17 19:52:55 +0100

    nav offset scroll test

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,7 @@ export default function Navbar() {
         to="hero"
         smooth={true}
         duration={500}
+        offset={-20}
         className="cursor-pointer justify-self-start">
         <img src={Logo} alt="Logo" className="w-10 sm:w-15 " />
       </ScrollLink>
@@ -17,6 +18,7 @@ export default function Navbar() {
         to="contact"
         smooth={true}
         duration={500}
+        offset={-20}
         className="cursor-pointer">
         Details
       </ScrollLink>
@@ -25,6 +27,7 @@ export default function Navbar() {
         to="gallery"
         smooth={true}
         duration={500}
+        offset={-20}
         className="cursor-pointer">
         Gallery
       </ScrollLink>
@@ -33,12 +36,14 @@ export default function Navbar() {
         to="guestlist"
         smooth={true}
         duration={500}
+        offset={-20}
         className="cursor-pointer">
         Guestlist
       </ScrollLink>
 
       <ScrollLink
         to="RSVP"
+        offset={-20}
         smooth={true}
         duration={500}
         className="cursor-pointer">
