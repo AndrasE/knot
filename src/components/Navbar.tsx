d8fcbbd7522fcc818d2d8e91fd93c4b6ff1478a9
AndrasE <rohadtsajt@gmail.com>
2025-09-18 19:13:41 +0100

    test nav font size mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Logo from "../assets/images/navlogo.avif";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 z-10 w-full flex items-center justify-between gap-2 px-2 py-4 text-lg border-b-1 border-stone-300  sm:px-10 md:px-20 bg-[#f5f0e6] ">
+    <nav className="fixed top-0 z-10 w-full flex items-center justify-between gap-2 px-2 py-3 text-lg border-b-1 border-stone-300  sm:px-10 md:px-20 bg-[#f5f0e6] ">
       {/* Hash links */}
       <HashLink
         to="/#hero"
@@ -15,23 +15,32 @@ export default function Navbar() {
           width="30"
           height="30"
           fetchPriority="high"
-          className="w-10 sm:w-15 "
+          className="w-10 "
         />
       </HashLink>
 
-      <HashLink to="/#details" smooth className="cursor-pointer">
+      <HashLink
+        to="/#details"
+        smooth
+        className="text-sm cursor-pointer sm:text-lg ">
         Details
       </HashLink>
 
-      <HashLink to="/#gallery" smooth className="cursor-pointer">
+      <HashLink
+        to="/#gallery"
+        smooth
+        className="text-sm cursor-pointer sm:text-lg ">
         Gallery
       </HashLink>
 
-      <HashLink to="/#guests" smooth className="cursor-pointer">
+      <HashLink
+        to="/#guests"
+        smooth
+        className="text-sm cursor-pointer sm:text-lg ">
         Guests
       </HashLink>
 
-      <HashLink to="/#RSVP" smooth className="cursor-pointer">
+      <HashLink to="/#RSVP" className="text-sm cursor-pointer sm:text-lg ">
         RSVP
       </HashLink>
     </nav>
