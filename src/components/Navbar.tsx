c4206670e5636589df9684113190dab2d08c8d59
AndrasE <rohadtsajt@gmail.com>
2025-09-18 19:16:27 +0100

    text base

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Logo from "../assets/images/navlogo.avif";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 z-10 w-full flex items-center justify-between gap-2 px-2 py-3 text-lg border-b-1 border-stone-300  sm:px-10 md:px-20 bg-[#f5f0e6] ">
+    <nav className="fixed top-0 z-10 w-full flex items-center justify-between gap-2 px-2 py-3 text-lg border-b-1 border-stone-300 sm:px-10 md:px-20 bg-[#f5f0e6] ">
       {/* Hash links */}
       <HashLink
         to="/#hero"
@@ -19,28 +19,32 @@ export default function Navbar() {
         />
       </HashLink>
 
+      {/* Change text-sm to text-base */}
       <HashLink
         to="/#details"
         smooth
-        className="text-sm cursor-pointer sm:text-lg ">
+        className="text-base cursor-pointer sm:text-lg ">
         Details
       </HashLink>
 
       <HashLink
         to="/#gallery"
         smooth
-        className="text-sm cursor-pointer sm:text-lg ">
+        className="text-base cursor-pointer sm:text-lg ">
         Gallery
       </HashLink>
 
       <HashLink
         to="/#guests"
         smooth
-        className="text-sm cursor-pointer sm:text-lg ">
+        className="text-base cursor-pointer sm:text-lg ">
         Guests
       </HashLink>
 
-      <HashLink to="/#RSVP" className="text-sm cursor-pointer sm:text-lg ">
+      <HashLink
+        to="/#RSVP"
+        smooth
+        className="text-base cursor-pointer sm:text-lg ">
         RSVP
       </HashLink>
     </nav>
