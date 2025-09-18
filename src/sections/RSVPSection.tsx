b4948170adf0436c3d80d697b03d455ff8abf7fb
AndrasE <rohadtsajt@gmail.com>
2025-09-18 12:03:01 +0100

    .env plus form

--- src/sections/RSVPSection.tsx
+++ src/sections/RSVPSection.tsx
@@ -2,11 +2,7 @@ import React, { useState } from "react";
 import { useFormspark } from "@formspark/use-formspark";
 
 export default function RSVPSection() {
-  const FORMSPARK_FORM_ID = process.env.REACT_APP_FORMSPARK_FORM_ID;
-
-  if (!FORMSPARK_FORM_ID) {
-    throw new Error("Formspark ID not found in environment variables");
-  }
+  const FORMSPARK_FORM_ID = import.meta.env.VITE_FORMSPARK_FORM_ID!;
 
   const [submit, submitting] = useFormspark({
     formId: FORMSPARK_FORM_ID,
@@ -29,39 +25,45 @@ export default function RSVPSection() {
       <h1 className="pb-5 text-6xl font-dawning ">RSVP </h1>
       <div className="flex flex-col gap-3 text-center ">
         <p className="max-w-md">
-          Please confirm your and/or your guest's attendance by filling out the
-          form below. Thank you!
+          Please confirm your attendance by filling out the form below.
         </p>
-        <form
-          onSubmit={onSubmit}
-          className="flex flex-col gap-4 p-5 rounded-md ">
+        <form onSubmit={onSubmit} className="flex flex-col gap-4 p-5 ">
           <input
             type="text"
+            id="name"
+            name="name"
             value={name}
             onChange={(e) => setName(e.target.value)}
             placeholder="Your Name"
-            className="p-2 border border-gray-300 rounded-md"
+            className="p-2 border rounded-md border-stone-300 focus:outline-none focus:ring-1 focus:ring-stone-500"
+            autoComplete="name"
             required
           />
           <input
             type="email"
+            id="email"
+            name="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Your Email"
-            className="p-2 border border-gray-300 rounded-md "
+            className="p-2 border rounded-md border-stone-300 focus:outline-none focus:ring-1 focus:ring-stone-500"
+            autoComplete="email"
             required
           />
           <textarea
+            id="description"
+            name="description"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
-            placeholder="A short description or message (e.g., plus one, dietary requirements)"
-            className="p-2 border border-gray-300 rounded-md"
-            rows={4} // Corrected: removed quotes around the number
+            placeholder="A short description or message (e.g., plus one, can`t come, etc.)"
+            className="p-2 border rounded-md border-stone-300 focus:outline-none focus:ring-1 focus:ring-stone-500"
+            autoComplete="off"
+            rows={4}
           />
           <button
             type="submit"
             disabled={submitting}
-            className="px-4 py-2 font-bold text-white transition duration-300 rounded-md bg-stone-800 hover:bg-stone-600 disabled:opacity-50">
+            className="px-4 py-2 ml-auto font-bold text-white transition duration-300 rounded-md max-w-fit bg-stone-500 hover:bg-stone-600 disabled:opacity-50">
             {submitting ? "Sending..." : "Send"}
           </button>
         </form>
