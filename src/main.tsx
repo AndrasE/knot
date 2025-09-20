228566ca128e1b03008da9eb18d0b927e5e497a6
AndrasE <rohadtsajt@gmail.com>
2025-09-20 19:06:07 +0100

    Revert "jumping fix on dvh-60px chrome mobile test"
    This reverts commit e6d71639d4723aa6f4d005922dbb0ccdb406a5eb.

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// ----------------------------
-// Fix mobile Chrome jumping
-// ----------------------------
-function setVh() {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-}
-
-window.addEventListener("resize", setVh);
-window.addEventListener("load", setVh);
-setVh(); // initial call
-
-// ----------------------------
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
