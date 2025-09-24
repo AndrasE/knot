d61917846b2056dcaf15334198fbbdfd78b53fd7
AndrasE <rohadtsajt@gmail.com>
2025-09-24 15:21:01 +0100

    Revert "Update App.tsx"
    This reverts commit 362f6b4bdf5c232883a141235ff8707569d4a148.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
@@ -6,6 +7,24 @@ import Details from "./pages/Details";
 import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker
+          .register("/sw.js")
+          .then((registration) => {
+            console.log(
+              "Service Worker registered with scope:",
+              registration.scope
+            );
+          })
+          .catch((error) => {
+            console.log("Service Worker registration failed:", error);
+          });
+      });
+    }
+  }, []);
+
   return (
     <Router>
       <ScrollToTop />
