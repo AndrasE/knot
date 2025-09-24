dc3f5f9c5c7387627ebd35d8442f1f7c457b22a0
AndrasE <rohadtsajt@gmail.com>
2025-09-24 16:40:35 +0100

    Update GameSection.tsx

--- src/sections/GameSection.tsx
+++ src/sections/GameSection.tsx
@@ -7,7 +7,7 @@ export default function GameSection() {
       to="/game"
       className="flex flex-col items-center justify-center m-auto transition-all duration-300 transform max-w-fit group text-stone-700 hover:scale-103 active:scale-96">
       <BillyIconSvg />
-      <p className="relative text-center transition-all duration-300 transform opacity-0 bottom-1 group-hover:scale-105 group-hover:opacity-100">
+      <p className="relative text-center transition-all duration-300 transform opacity-80 md:opacity-0 bottom-1 group-hover:scale-105 group-hover:opacity-100">
         Hello, do you want to play a game?
       </p>
     </Link>
