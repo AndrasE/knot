1f07ded86ca1c047de3db8b4989a9348ec9196a0
Andras E <rohadtsajt@gmail.com>
2025-11-23 17:47:38 +0000

    detail update

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -3,38 +3,53 @@ export default function DetailsPage() {
     <div className="max-w-2xl pt-20 pb-5 mx-auto flex-center-100vh">
       <div className="pl-4 border-l-2 border-stone-500">
         <div className="text-left ">
-          <p className="circle-dot">Ceremony: Morden Park House - The Gazebo</p>
-          <p className="pb-5 text-justify">
-            Morden Park House, a stunning Grade II listed Georgian manor, has
-            been home to the Merton Register Office since November 2000.
-            Originally built in 1770 by John Ewart, this historic building is
-            nestled within Morden Park.
+          <p className="pb-2 circle-dot">Tying the knot at Morden Park House</p>
+          <p className="pb-2 text-justify">
+            This is a beautiful Registry Office in the south of London where we
+            will say "yes" in the garden under the Gazebo (please all keep your
+            fingers crossed for fantastic weather!).
           </p>
-          <p className="circle-dot">Celebration: Cherry Tree - Dulwich</p>
-          <p className="text-justify">
-            TBA - The Cherry Tree is a charming gastropub located in the heart
-            of Dulwich Village. Known for its warm and inviting atmosphere, it
-            offers a delightful menu featuring seasonal British cuisine and an
-            extensive selection of drinks. With its cozy interiors and beautiful
-            outdoor seating area, The Cherry Tree provides a perfect setting for
-            a memorable celebration with friends and family.
+          <p className="pb-2 text-justify">
+            The ceremony starts at 2PM, please be there around 30 minutes
+            earlier.
           </p>
-
-          <p className="pb-5 text-justify">
-            TBA - The Cherry Tree is a charming gastropub located in the heart
-            of Dulwich Village. Known for its warm and inviting atmosphere, it
-            offers a delightful menu featuring seasonal British cuisine and an
-            extensive selection of drinks. With its cozy interiors and beautiful
-            outdoor seating area, The Cherry Tree provides a perfect setting for
-            a memorable celebration with friends and family.
+          <p className="pb-2 text-center">
+            Address: <br />
+            Morden Park House <br /> London Road Morden <br />
+            SM4 5QU
           </p>
-          <p className="circle-dot">After?: TBA</p>
-          <p className="pb-5 text-justify">
-            No strict plans — just city adventures and a dash of improv!
+          <p className="pb-4 text-justify">
+            If you come by train: Take the Northern Line (Black Line) from
+            London Bridge, direction "Morden" Get off at Morden station (last
+            stop) Take the bus 93 from Stop G, direction "North Cheam" Get off 4
+            stops later at "Merton College" Cross the road and walk 5 min
+            through the park.
           </p>
-          <p className="circle-dot">Accomodation</p>
+          <p className="pb-2 circle-dot">
+            Commuting to the Pub in a Classic 1960s Routemaster Bus!
+          </p>
+          <p className="pb-4 text-justify">
+            Get ready for a uniquely London experience! A shiny red vintage
+            Routemaster bus will be waiting to pick up all guests at 3:30 PM
+            sharp, providing a memorable ride straight to the pub where the
+            celebration will continue. It's the perfect way to travel in style
+            and keep the party atmosphere going!
+          </p>
+          <p className="circle-dot">Cherry Three Pub</p>
+          <p className="pb-2 text-justify">
+            Drinking, Dinnering, Dancing in the pub We will start with drinks
+            and snacks at the Cherry Tree pub in Dulwich, and will later indulge
+            in a little buffet.
+          </p>
+          <p className="pb-2 text-justify">
+            The pub is located in pretty Dulwich, 31-33 Grove Vale, SE22 8EQ
+            London.
+          </p>
           <p className="text-justify ">
-            No strict plans — just city adventures and a dash of improv!
+            Look forward to a casual pub vibe where we have our own dedicated
+            area outside and partly shielded, but can also mingle with the pub
+            crowd. <br /> As our fundings are small, we would ask you to pay for
+            your drinks.
           </p>
         </div>
       </div>
