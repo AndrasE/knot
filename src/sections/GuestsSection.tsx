901ca871eea4927f4a50333ead9577d206976d45
Andras E <rohadtsajt@gmail.com>
2025-11-29 14:35:22 +0000

    Update GuestsSection.tsx

--- src/sections/GuestsSection.tsx
+++ src/sections/GuestsSection.tsx
@@ -13,26 +13,12 @@ export default function GuestsSection() {
         <ul className="grid w-full grid-cols-2 gap-x-10 gap-y-2 md:grid-cols-3 lg:grid-cols-4">
           <li>Sarah </li>
           <li>Andras </li>
-          <li>Anyu</li>
-          <li>Apu </li>
+          <li>Mama A</li>
+          <li>Pappa A </li>
+          <li>Pappa S </li>
           <li>Kari </li>
           <li>Boch </li>
           <li>Dave</li>
-          <li>Dorian</li>
-          <li>Persephone</li>
-          <li>Lysander</li>
-          <li>Isolde</li>
-          <li>Finnian</li>
-          <li>Cassiopeia</li>
-          <li>Zephyr</li>
-          <li>Thalia</li>
-          <li>Caspian</li>
-          <li>Ophelia</li>
-          <li>Orion</li>
-          <li>Evander</li>
-          <li>Rowan</li>
-          <li>Astraia</li>
-          <li>Faelan</li>
         </ul>
       </div>
     </section>
